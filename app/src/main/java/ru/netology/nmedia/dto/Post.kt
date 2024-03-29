fd7c21e9dcbbdf98589d1ab8bc5e5eb08f036de6
chernyaevpavel <pawliklist@yandex.ru>
2024-03-29 13:13:13 +0400

    change var to val in Post class

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -4,9 +4,9 @@ data class Post(
     val id: Long,
     val author: String,
     val content: String,
+    val likedByMe: Boolean = false,
     val published: String,
-    var likedByMe: Boolean = false,
-    var likeCount: Int = 0,
-    var shareCount: Int = 0,
-    var visibilityCount: Int = 0
+    val likeCount: Int = 0,
+    val shareCount: Int = 0,
+    val visibilityCount: Int = 0
 )
