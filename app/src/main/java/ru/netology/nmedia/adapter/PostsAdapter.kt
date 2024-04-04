02dbda9680f759b74b44bf9345bf970be0f615f7
chernyaevpavel <pawliklist@yandex.ru>
2024-04-04 21:55:46 +0400

    add recycler view

---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostsAdapter.kt
@@ -0,0 +1,71 @@
+package ru.netology.nmedia.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.extension.getFormatedNumber
+
+typealias OnLikeListener = (post: Post) -> Unit
+typealias OnShareListener = (post: Post) -> Unit
+
+class PostsAdapter(
+    private val onLikeListener: OnLikeListener,
+    private val onShareListener: OnShareListener
+) : ListAdapter<Post, PostViewHolder>(PostDiffCallback()){
+
+
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        val post = getItem(position)
+        holder.bind(post)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = CardPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return  PostViewHolder(binding, onLikeListener, onShareListener)
+    }
+}
+
+class PostViewHolder(
+    private val binding: CardPostBinding,
+    private val onLikeListener: OnLikeListener,
+    private val onShareListener: OnShareListener
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(post: Post) {
+        binding.apply {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            like.setImageResource(
+                if (post.likedByMe) R.drawable.baseline_favorite_24_red else R.drawable.baseline_favorite_border_24
+            )
+            likeCount.text = post.likeCount.getFormatedNumber()
+            shareCount.text = post.shareCount.getFormatedNumber()
+            visibilityCount.text = post.visibilityCount.getFormatedNumber()
+
+            like.setOnClickListener {
+                onLikeListener(post)
+            }
+
+            share.setOnClickListener {
+                onShareListener(post)
+            }
+        }
+    }
+}
+
+class PostDiffCallback: DiffUtil.ItemCallback<Post>() {
+    override fun areItemsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
+        return oldItem == newItem
+    }
+
+}
