5d6071848a23ab32c352eb11dbf696174df77c57
chernyaevpavel <pawliklist@yandex.ru>
2024-04-19 12:48:46 +0400

    bug fix null value for empty list

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFileImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryFileImpl.kt
@@ -62,12 +62,13 @@ class PostRepositoryFileImpl(
         )
     )
     private val data = MutableLiveData(posts)
+
     init {
         val file = context.filesDir.resolve(FILENAME)
         if (file.exists()) {
             context.openFileInput(FILENAME).bufferedReader().use {
                 posts = gson.fromJson(it, typeToken)
-                nextId = posts.maxOf { it.id } + 1
+                nextId = (posts.maxOfOrNull { it.id } ?: 0) + 1
             }
         } else {
             posts = defaultPosts
