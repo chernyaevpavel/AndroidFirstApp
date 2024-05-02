2ca7e7f83562e7557585d3fc43723106a2c3ca25
chernyaevpavel <pawliklist@yandex.ru>
2024-05-02 17:28:03 +0400

    conversion activity to fragment

---
+++ app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
@@ -0,0 +1,53 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.databinding.FragmentEditPostBinding
+import ru.netology.nmedia.utils.LongArg
+import ru.netology.nmedia.utils.StringArg
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+class EditPostFragment : Fragment() {
+
+    companion object {
+        var Bundle.textArg: String? by StringArg
+        var Bundle.longArg: Long by LongArg
+    }
+
+    private val postViewModel: PostViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentEditPostBinding.inflate(
+            inflater, container, false
+        )
+        binding.save.setOnClickListener {
+            postViewModel.changeContentAndSave(binding.content.text.toString())
+            postViewModel.setEmptyPost()
+            findNavController().navigateUp()
+        }
+
+        binding.cancel.setOnClickListener() {
+            postViewModel.setEmptyPost()
+            findNavController().navigateUp()
+        }
+
+        arguments?.textArg?.let {
+            val content = it
+            if (it != null) {
+                binding.content.setText(content)
+                binding.editInfo.visibility = View.VISIBLE
+            }
+        }
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/ru/netology/nmedia/activity/PostFragment.kt
@@ -0,0 +1,72 @@
+package ru.netology.nmedia.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.netology.nmedia.R
+import ru.netology.nmedia.activity.EditPostFragment.Companion.longArg
+import ru.netology.nmedia.activity.EditPostFragment.Companion.textArg
+import ru.netology.nmedia.adapter.OnInteractionListener
+import ru.netology.nmedia.adapter.PostViewHolder
+import ru.netology.nmedia.databinding.CardPostBinding
+import ru.netology.nmedia.databinding.FragmentPostBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewmodel.PostViewModel
+
+
+class PostFragment : Fragment() {
+    private val postViewModel: PostViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = CardPostBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        postViewModel.data.observe(viewLifecycleOwner) { posts ->
+            val post = posts.find { it.id == arguments?.longArg }?:return@observe
+            val holder = PostViewHolder(binding, object : OnInteractionListener{
+                override fun onLike(post: Post) {
+                    postViewModel.likeById(post.id)
+                }
+
+                override fun onShare(post: Post) {
+                    postViewModel.shareById(post.id)
+                }
+
+                override fun onRemove(post: Post) {
+                    postViewModel.removeById(post.id)
+                    findNavController().navigateUp()
+                }
+
+                override fun onEdit(post: Post) {
+                    postViewModel.edit(post)
+                    findNavController().navigate(
+                        R.id.action_postFragment_to_editPostFragment,
+                        Bundle().apply {
+                            textArg = post.content
+                        })
+                }
+
+                override fun onPlay(post: Post) {
+                    startActivity(postViewModel.prepareVideoIntent(post))
+                }
+
+                override fun onOpenCardPost(post: Post) {
+                }
+            })
+            holder.bind(post)
+        }
+        return binding.root
+    }
+
+}
